c0554d4218e6c21d80a4ec107d15a531b04033f5
zGiusee <giusebelforte@gmail.com>
2023-11-20 14:40:47 +0100

    [UPDATE] js code done style remainning

--- js/script.js
+++ js/script.js
@@ -5,13 +5,29 @@ let eta = prompt("Inserisci la tua età.")
 let km = prompt("inserisci la quantità di km che vuoi percorrere.")
 km = km * 0.21;
 
+let sconto;
+let ticketPrice = km;
 
-
-if(eta < 65){
-
-    
+if(eta > "65"){
+    sconto = ticketPrice * 40 / 100;
+    ticketPrice = km - sconto;
 }
+else{
+    ticketPrice = ticketPrice;
+}
+
+
+if(eta < "18"){
+    sconto = ticketPrice * 20 / 100;
+    ticketPrice = km - sconto;
+}
+else{
+    ticketPrice = ticketPrice;
+}
+
 
 
 console.log(km)
 console.log(eta)
+console.log(ticketPrice)
+console.log(Sconto)
