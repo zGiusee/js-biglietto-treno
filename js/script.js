95572fe7aaa37e49bf022bc1d9d2d84db86c29bf
zGiusee <giusebelforte@gmail.com>
2023-11-20 14:10:27 +0100

    [UPDATE] first part of java done

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// PROMPT COMMANDS
+
+let eta = prompt("Inserisci la tua età.")
+
+let km = prompt("inserisci la quantità di km che vuoi percorrere.")
+km = km * 0.21;
+
+
+
+if(eta < 65){
+
+}
+
+
+console.log(km)
+console.log(eta)
