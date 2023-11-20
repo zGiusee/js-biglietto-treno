9d165b279c2623d211c3f9f560091274df7086f6
zGiusee <giusebelforte@gmail.com>
2023-11-20 16:35:44 +0100

    [update] Exercise 100% done

--- js/script.js
+++ js/script.js
@@ -13,26 +13,23 @@ let message;
 if(eta >= 65){
     sconto = ticketFullPrice * 40 / 100;
     ticketFullPrice = km - sconto;
-    // ARROTONDO IL PREZZO FINALE
-    ticketFullPrice = ticketFullPrice.toFixed(0);
     // AGGIUNGO UN MESSAGGIO DI AVVISO
     message = "Complimenti rientri nella categoria degli Over.65 e questo ti garantisce uno sconto del 40%! Il tuo prezzo finale sarà:"
 }
 else if(eta < 18){
     sconto = ticketFullPrice * 20 / 100;
     ticketFullPrice = km - sconto;
-    // ARROTONDO IL PREZZO FINALE
-    ticketFullPrice = ticketFullPrice.toFixed(0);
     // AGGIUNGO UN MESSAGGIO DI AVVISO
     message = "Complimenti rientri nella categoria Under.18 e questo ti garantisce uno sconto del 20%! Il tuo prezzo finale sarà:"
 
 }
 else{
-    // ARROTONDO IL PREZZO FINALE
-    ticketFullPrice = ticketFullPrice.toFixed(0);
     // AGGIUNGO UN MESSAGGIO DI AVVISO
     message = "Ecco il tuo prezzo finale."
 }
+
+// ARROTONDO IL PREZZO FINALE DEL BIGLIETTO
+ticketFullPrice = ticketFullPrice.toFixed(2);
 
 // INDICO IL RISULTATO NEL FILE HTML
 
