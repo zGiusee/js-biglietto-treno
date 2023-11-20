dc97745ce12fe895ca56bc2573749fd3bfd3254c
zGiusee <giusebelforte@gmail.com>
2023-11-20 15:11:28 +0100

    [UPDATE] js part 100% done details remaining

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
-// PROMPT COMMANDS
-
+// RICHIEDO LE INFORMAZINI NEL PROMPT
 let eta = prompt("Inserisci la tua età.")
 
 let km = prompt("inserisci la quantità di km che vuoi percorrere.")
 km = km * 0.21;
 
+// DEFINISCO LE VARIABILI
 let sconto;
-let ticketPrice = km;
+let ticketFullPrice = km;
+let message;
 
-if(eta > "65"){
-    sconto = ticketPrice * 40 / 100;
-    ticketPrice = km - sconto;
+// ESEGUO I CALCOLI
+if(eta >= "65"){
+    sconto = ticketFullPrice * 40 / 100;
+    ticketFullPrice = km - sconto;
+    // ARROTONDO IL PREZZO FINALE
+    ticketFullPrice = ticketFullPrice.toFixed(0)
+    // AGGIUNGO UN MESSAGGIO DI AVVISO
+    message = "Complimenti rientri nella categoria degli Over.65 e questo ti garantisce uno sconto del 40%! Il tuo prezzo finale sarà:"
+}
+else if(eta < "18"){
+    sconto = ticketFullPrice * 20 / 100;
+    ticketFullPrice = km - sconto;
+    // ARROTONDO IL PREZZO FINALE
+    ticketFullPrice = ticketFullPrice.toFixed(0)
+    // AGGIUNGO UN MESSAGGIO DI AVVISO
+    message = "Complimenti rientri nella categoria Under.18 e questo ti garantisce uno sconto del 20%! Il tuo prezzo finale sarà:"
+
 }
 else{
-    ticketPrice = ticketPrice;
+    // ARROTONDO IL PREZZO FINALE
+    ticketFullPrice = ticketFullPrice.toFixed(0)
+    // AGGIUNGO UN MESSAGGIO DI AVVISO
+    message = "Ecco il tuo prezzo finale."
 }
 
+// INDICO IL RISULTATO NEL FILE HTML
 
-if(eta < "18"){
-    sconto = ticketPrice * 20 / 100;
-    ticketPrice = km - sconto;
-}
-else{
-    ticketPrice = ticketPrice;
-}
-
-
+document.getElementById(`ticketprice`).innerHTML= ticketFullPrice;
+document.getElementById(`avvisosconto`).innerHTML= message;
 
 console.log(km)
 console.log(eta)
-console.log(ticketPrice)
+console.log(ticketFullPrice)
 console.log(Sconto)
